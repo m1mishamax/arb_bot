1a87f7f120065f6a869407743c1d67bff6fb455f
fremeralinego <holmora@gmail.com>
2023-05-15 03:02:52 +0400

    Update code to version v%new_version% with historical price data and new arbitrage calculator module

---
+++ config.py
@@ -0,0 +1,10 @@
+api_key_binance = '1LVUDp2y94SmGpBUipZQHSRpz65mdPy3Ovhz2CZOYTBpA5hpeZBXgNXZ4mG2Kc8K'
+secret_key_binance = 'R2jH9LT7MtrpIs5jGZtuadUzRSXzR9TmS8R57Mcbb8wZLTCylUvzjfIifWM3qa1k'
+
+api_key_bybit = 'ciXK8ohtCniimqpcC8'
+secret_key_bybit = 'm1zVbVf1NFgN8MsvW90Rm9WFMRRdA7NnlluY'
+
+ARBITRAGE_THRESHOLD = 0.25
+MAX_POSITIONS_PER_PAIR = 1
+MAX_TOTAL_POSITIONS = 1
+PERCENT_ACCEPTANCE = 0.05 # This means that additional bonus percent. if threshold 0.3 and PERCENT_ACCEPTANCE 0.1 than profit summ should be 0.4
